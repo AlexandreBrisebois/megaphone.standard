83705afaeabe084a0d619720793b11d5a3211a2f
Alexandre Brisebois <brisebois@outlook.com>
2021-03-17 12:56:31 -0400

    fix clock

--- src/megaphone.standard/Time/UtcClock.cs
+++ src/megaphone.standard/Time/UtcClock.cs
@@ -4,6 +4,6 @@ namespace Megaphone.Standard.Time
 {
     public class UtcClock : IClock
     {
-        public DateTimeOffset Now { get; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset Now => DateTimeOffset.UtcNow;
     }
 }
